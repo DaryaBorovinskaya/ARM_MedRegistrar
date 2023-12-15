4e2896b7554bfe771a04fe6a274b9616dcb08cf8
DaryaBorovinskaya <dashh17@gmail.com>
2023-12-15 21:06:16 +0700

    работа с MVP

---
+++ ARM_MedRegistrar/Model/Persons/IUserEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARM_MedRegistrar.Model.Persons
+{
+    public interface IUserEmployee
+    {
+        public IFullName FullName { get; set; }
+        public string Post { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ ARM_MedRegistrar/Presenter/AddDoctorPresenter.cs
@@ -0,0 +1,30 @@
+﻿using ARM_MedRegistrar.Model.Json.DoctorRepository;
+using ARM_MedRegistrar.Model.Persons;
+using ARM_MedRegistrar.View.AddDoctor;
+using ARM_MedRegistrar.Model.Json.DoctorRepository;
+
+namespace ARM_MedRegistrar.Presenter
+{
+    public class AddDoctorPresenter
+    {
+        IAddDoctorForm _view;
+        FullName _fullName;
+        Doctor _newDoctor;
+        IDoctorRepository _jsonDoctorRepository;
+        public AddDoctorPresenter(IAddDoctorForm view)
+        {
+            _view = view;
+        }
+
+        
+
+        public void AddDoctor()
+        {
+            _fullName = new(_view.Surname, _view.Name, _view.Patronymic);
+            _newDoctor = new(_fullName, _view.PhoneNumber, _view.Specializations, _view.PlotNumber, _view.Cabinet);
+
+            _jsonDoctorRepository = new JsonDoctorRepository("doctors.json");
+            _jsonDoctorRepository.Add(_newDoctor);
+        }
+    }
+}
